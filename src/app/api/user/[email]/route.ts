89b8c208334a16439b06c0073a47f54bea5a6db8
Harshdeep Singh <harshdeepsingh1121@outlook.com>
2025-08-22 23:42:14 +0530

    Add new API endpoint to fetch user details by email

---
+++ src/app/api/user/[email]/route.ts
@@ -0,0 +1,29 @@
+import { connectToDatabase } from "@/lib/db";
+import { User } from "@/models/User";
+import { NextResponse } from "next/server";
+
+
+export async function GET(request: Request, context: { params: { email: string } }) {
+    const { params } = context;
+    const { email } = await params; // 👈 This is the key change
+
+    try {
+        await connectToDatabase();
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return NextResponse.json({ error: "User not found" }, { status: 404 });
+        }
+
+        return NextResponse.json({
+            name: user.username,
+            email: user.email,
+            profilePicture: user.profilePicture,
+            isAdmin: user.isAdmin,
+
+        });
+    } catch (error) {
+        console.error("Error fetching user:", error);
+        return NextResponse.json({ error: "Internal server error" }, { status: 500 });
+    }
+}
