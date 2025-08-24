6bf0a0597615817ae26bd5a72e23889200173e47
Harshdeep Singh <harshdeepsingh1121@outlook.com>
2025-08-24 21:57:25 +0530

    Fix lastMessage uppdate in conversation

--- src/lib/services/message.service.ts
+++ src/lib/services/message.service.ts
@@ -2,6 +2,7 @@
 import * as messageRepo from "@/lib/repositories/message.repo";
 import { CreateMessageInput } from "../validators/ message.schema";
 import { Types } from "mongoose";
+import { Conversation } from "@/models/Conversation";
 
 export async function createMessage(data: CreateMessageInput) {
     // correctly map senderId → sender
@@ -9,9 +10,15 @@ export async function createMessage(data: CreateMessageInput) {
         sender: new Types.ObjectId(data.senderId),
         conversationId: new Types.ObjectId(data.conversationId),
         content: data.content,
-        // if you have messageType in schema, include here:
-        // messageType: data.messageType ?? "text",
+        messageType: data.messageType ?? "text",
     };
+    const conversation = await Conversation.findById(data.conversationId);
+    if (!conversation) {
+        throw new Error("Conversation not found");
+    }
+    conversation.lastMessage = toSave;
+    conversation.lastMessage._creationTime = new Date();
+    await conversation.save();
 
     console.log("🔊 [Service] Emitting to room", data.conversationId, toSave);
     return await messageRepo.saveMessage(toSave);
