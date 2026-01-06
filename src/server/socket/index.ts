1451891a0d3cb1fb4dc9407918002e26848a68f5
Harshdeep Singh <harshdeepsingh1121@outlook.com>
2026-01-06 20:35:59 +0530

    feat: integrate message edit and delete handlers into socket connection for enhanced message management

--- src/server/socket/index.ts
+++ src/server/socket/index.ts
@@ -1,11 +1,14 @@
 import { initRedis } from "./redis";
-import http from "http";
 import { initIO } from "./io";
 import { socketAuth } from "./middleware/auth";
+import messageEditHandler from "./handlers/message/edit.handler";
+import { DeleteHandler } from "./handlers/message/delete.handler";
 import { adminHandler } from "./handlers/admin/admin";
 import { presenceHandler } from "./handlers/presence/presence.handler";
-import { typingHandler } from "./handlers/typing/typing.handler";
 import { registerMessageHandlers } from "./handlers/message/message.handler";
+
+import { typingHandler } from "./handlers/typing/typing.handler";
+//import { Server as SocketIOServer } from "socket.io";
 
 export async function initSocket(server: any) {
     const redis = await initRedis();
@@ -14,9 +17,13 @@ export async function initSocket(server: any) {
     io.use(socketAuth);
 
     io.on("connection", (socket) => {
+        console.log("🔌 socket connected:", socket.id);
         adminHandler(io, socket, redis);
-        presenceHandler(io, socket, redis);
+        presenceHandler(io, socket);
+        registerMessageHandlers(io, socket);
         typingHandler(io, socket);
-        registerMessageHandlers(io, socket, redis);
+        registerMessageHandlers(io, socket);
+        messageEditHandler(io, socket);
+        DeleteHandler(io, socket);
     });
 }
