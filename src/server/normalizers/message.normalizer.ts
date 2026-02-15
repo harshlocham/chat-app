763f927e326bc03e09bb38376bb628b0ef089673
Harshdeep Singh <harshdeepsingh1121@outlook.com>
2026-02-15 21:46:53 +0530

    feat(server): implement DTO-based message normalization from DB to transport layer

--- src/server/normalizers/message.normalizer.ts
+++ src/server/normalizers/message.normalizer.ts
@@ -1,21 +1,51 @@
+import { IMessagePopulated } from "@/models/Message.js";
 import type { ClientMessage } from "../../shared/types/client-message.js";
 
 // src/server/normalizers/message.normalizer.ts
-export function normalizeMessage(doc: any): ClientMessage {
+export function normalizeMessage(doc: IMessagePopulated): ClientMessage {
     return {
         _id: doc._id.toString(),
         conversationId: doc.conversationId.toString(),
-        sender: doc.sender.toString(),
 
-        content: doc.isDeleted ? null : doc.content,
+        content: doc.content,
         messageType: doc.messageType,
 
-        createdAt: doc.createdAt.toISOString(),
+        sender: {
+            _id: doc.sender._id.toString(),
+            username: doc.sender.username,
+            profilePicture: doc.sender.profilePicture,
+        },
+
+        repliedTo: doc.repliedTo
+            ? {
+                _id: doc.repliedTo._id.toString(),
+                content: doc.repliedTo.content,
+                sender: {
+                    _id: doc.repliedTo.sender._id.toString(),
+                    username: doc.repliedTo.sender.username,
+                    profilePicture: doc.repliedTo.sender.profilePicture,
+                },
+            }
+            : null,
+
+        reactions: Array.isArray(doc.reactions)
+            ? doc.reactions.map((reaction) => ({
+                emoji: reaction.emoji,
+                users: (reaction.users ?? []).map((user: any) =>
+                    typeof user === "string"
+                        ? user
+                        : user._id
+                            ? user._id.toString()
+                            : user.toString()
+                ),
+            }))
+            : undefined,
+
+        createdAt: doc.createdAt,
+        editedAt: doc.isEdited ? doc.updatedAt?.toISOString() : undefined,
+        // deletedAt: doc.isDeleted ? doc.updatedAt?.toISOString() : undefined,
+
         isEdited: doc.isEdited,
         isDeleted: doc.isDeleted,
-
-        reactions: doc.reactions ?? [],
-        deliveredTo: doc.deliveredTo?.map(String) ?? [],
-        seenBy: doc.seenBy?.map(String) ?? [],
     };
 }
