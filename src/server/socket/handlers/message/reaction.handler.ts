d9468f7617df88824b5e1c88fb4a6d8de74cff63
Harshdeep Singh <harshdeepsingh1121@outlook.com>
2026-03-13 21:42:05 +0530

    refactor: remove unused reaction handler for cleaner codebase

--- src/server/socket/handlers/message/reaction.handler.ts
+++
@@ -1,9 +0,0 @@
-// src/server/socket/handlers/message/reaction.handler.ts
-import { Server, Socket } from "socket.io";
-
-export default function reactionHandler(io: Server, socket: Socket) {
-    socket.on("message:react", async (message) => {
-
-        io.to(message.conversationId.toString()).emit("message:reaction:updated", message);
-    });
-}
