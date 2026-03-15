f9fb481a0484a827b60e558367dcf09aa7104a9f
Harshdeep Singh <harshdeepsingh1121@outlook.com>
2026-03-15 20:15:47 +0530

    feat: update admin socket handler to use SocketEvents for event names

--- src/server/socket/handlers/admin/admin.ts
+++ src/server/socket/handlers/admin/admin.ts
@@ -1,19 +1,20 @@
 import { Server, Socket } from "socket.io";
 import { RedisAdapterClients } from "../../redis.js";
+import { SocketEvents } from "../../../../shared/types/SocketEvents.js";
 
 const PRESENCE_KEY = "active_users";
 const MESSAGE_COUNT_KEY = "total_messages_today";
 
 export function adminHandler(io: Server, socket: Socket, redis: RedisAdapterClients) {
-    socket.on("admin:join", async () => {
+    socket.on(SocketEvents.ADMIN_JOIN, async () => {
         socket.join("admins");
 
         const [activeUsers, totalMessagesToday] = await Promise.all([
-            redis.pubClient.sCard(PRESENCE_KEY),
+            redis.pubClient.scard(PRESENCE_KEY),
             redis.pubClient.get(MESSAGE_COUNT_KEY),
         ]);
 
-        socket.emit("dashboard:init", {
+        socket.emit(SocketEvents.DASHBOARD_INIT, {
             activeUsers,
             totalMessagesToday: Number(totalMessagesToday) || 0,
         });
